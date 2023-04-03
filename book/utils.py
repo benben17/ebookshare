2e646df5531ec4b6a341f86b3386242aff241c94
leezhua17 <892100089@qq.com>
2023-04-03 16:00:59 +0800

    fix

--- book/utils.py
+++ book/utils.py
@@ -8,7 +8,6 @@ from pathlib import Path
 import xml.etree.ElementTree as ET
 import isbnlib
 import requests
-from idna import unicode
 from requests.exceptions import RequestException
 
 import config
