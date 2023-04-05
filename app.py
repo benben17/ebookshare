f5072335f41c7025271456a2d257d1aaf2ebd73e
leezhua17 <892100089@qq.com>
2023-04-05 14:42:33 +0800

    V1.0.2

--- app.py
+++ app.py
@@ -6,5 +6,4 @@ from book.schedule import *
 
 
 if __name__ == '__main__':
-    app.logger.info("aaa")
     app.run(debug=True,threaded=True,use_reloader=False)
--- book/schedule.py
+++ book/schedule.py
@@ -78,7 +78,7 @@ scheduler = APScheduler(BackgroundScheduler())
 scheduler.init_app(app)
 scheduler.add_job(id="delete_file", func=delete_file_out_24_hours, trigger="interval", hours=2, replace_existing=False)
 scheduler.add_job(id="send_file", func=book_send, args=("0"), trigger="interval", seconds=180, replace_existing=False,
-                  max_instances=2)
+                  max_instances=3)
 scheduler.add_job(id="retry_send_file", func=book_send, args=("4"), trigger="cron", day="*", hour="01",
                   replace_existing=False)
 scheduler.start()
