8561794ac73d39aec6bf823c5571ef72bef4da1b
leezhua17 <892100089@qq.com>
2023-08-19 00:43:10 +0800

    bug fix

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 # from book.views import user, feed, wechat, myfeed
 import os
-
 from book import app
-
 from gevent import pywsgi
 
 if __name__ == '__main__':
     # print(app.url_map)
-    print(os.getenv('env'))
     app.run(debug=True, threaded=True, use_reloader=False, port=5000)
     # server = pywsgi.WSGIServer(('0.0.0.0',8000),app)
     # server.serve_forever()
