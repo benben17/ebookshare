3adc49ffa313e7eceb5a762f7b252ccbde9af77c
leezhua17 <892100089@qq.com>
2023-08-19 01:31:13 +0800

    bug fix

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ from gevent.pywsgi import WSGIServer
 
 if __name__ == '__main__':
     # print(app.url_map)
-    http_server = WSGIServer(('0.0.0.0', os.getenv("PORT")), app)
+    http_server = WSGIServer(('0.0.0.0', int(os.getenv("PORT"))), app)
     http_server.serve_forever()
